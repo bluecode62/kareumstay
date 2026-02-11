e798851d32c33e26bce23698b80c6fe5e42113bc
baeyunju <byj622@gmail.com>
2026-02-11 11:51:55 +0900

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import './App.css';
-import styled from 'styled-components';
+import "./App.css";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
-import Gnb from './components/Gnb';
-import Footer from './components/Footer';
+import Gnb from "./components/Gnb";
+import Footer from "./components/Footer";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: "Noto Sans KR", sans-serif;
+  }
+`;
+const Layout = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
   return (
     <div className="App">
-    <Gnb />
-    <wrap>
-
-    </wrap>
-    <Footer />
+      <GlobalStyle />
+      <Layout>
+        <Gnb />
+        <div>주요 내용들</div>
+        <Footer />
+      </Layout>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,108 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
+const Footer = styled.div`
+  margin-top: auto;
+  background-color: #4b4f53;
+  color: white;
+`;
+const Inside = styled.div`
+  width: 1070px;
+  height: 150px;
+  padding: 40px;
+  margin: 0 auto;
+  display: flex;
+  gap: 20px;
+`;
+const LeftBox = styled.div`
+  width: 120px;
+  height: 150px;
+`;
+const RightBox = styled.div`
+  width: 900px;
+  height: 100px;
+`;
+const ImgBox = styled.div`
+  width: 120px;
+`;
+const ImgList = styled.div`
+  width: 500px;
+  height: 40px;
+  display: flex;
+  gap: 20px;
+`;
+const LogoItem = styled.div`
+  width: 150px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const LogoImg = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+`;
+const FooterInfo = styled.div`
+  margin-top: 10px;
+  font-size: 14px;
+  text-align: left;
+`;
+const Line = styled.p`
+  margin: 3px 0;
+`;
 
 export default function footer() {
   return (
-    <div>
-      <div>Footer 영역</div>
-    </div>
-  )
+    <Footer>
+      <Inside>
+        <LeftBox>
+          <ImgBox type="left">
+            <img
+              src={process.env.PUBLIC_URL + "../images/ft_logo.png"}
+              alt="로고 이미지"
+            />
+          </ImgBox>
+        </LeftBox>
+        <RightBox>
+          <ImgList>
+            <LogoItem>
+              <LogoImg
+                src={process.env.PUBLIC_URL + "../images/jeju_logo.png"}
+                alt="푸터 이미지01"
+              />
+            </LogoItem>
+            <LogoItem>
+              <LogoImg
+                src={process.env.PUBLIC_URL + "../images/jejutourism_logo.png"}
+                alt="푸터 이미지01"
+              />
+            </LogoItem>
+            <LogoItem>
+              <LogoImg
+                src={process.env.PUBLIC_URL + "../images/visitjeju_logo.png"}
+                alt="푸터 이미지01"
+              />
+            </LogoItem>
+          </ImgList>
+          <FooterInfo>
+            <Line>
+              제주관광공사 JEJU TOURISM ORGANIZATION | 사장 고승철 |
+              개인정보관리책임자 문성환 | 사업자등록번호 616-82-21432
+            </Line>
+            <Line>
+              ​(63122) 제주특별자치도 제주시 선덕로 23(연동) 제주웰컴센터
+            </Line>
+            <Line>
+              제주관광정보센터 TEL (064)740-6000 | FAX (064)740-6090~1 | 문의 :
+              kareumstay@ijto.or.kr​
+            </Line>
+            <Line>
+              COPYRIGHT ⓒ JEJU TOURISM ORGANIZATION. ALL RIGHTS RESERVED.
+            </Line>
+          </FooterInfo>
+        </RightBox>
+      </Inside>
+    </Footer>
+  );
 }
